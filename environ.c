a6d148b0e44bef7181631fed79724c3fd9e26882
Bright <brightadams58@gmail.com>
2023-04-26 12:14:08 -0700

    new updateds

--- environ.c
+++ environ.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _myenv - Prints the current environment.
- * @info: Pointer to struct containing potential arguments.
- *
- * Return: Always 0.
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
 int _myenv(info_t *info)
 {
@@ -13,39 +13,39 @@ int _myenv(info_t *info)
 }
 
 /**
- * _getenv - Gets the value of an environment variable.
- * @info: Pointer to struct containing potential arguments.
- * @name: Name of environment variable.
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
  *
- * Return: Pointer to value of environment variable, or NULL if not found.
+ * Return: the value
  */
 char *_getenv(info_t *info, const char *name)
 {
-	list_t *env_node = info->env;
+	list_t *node = info->env;
 	char *p;
 
-	while (env_node)
+	while (node)
 	{
-		p = starts_with(env_node->str, name);
+		p = starts_with(node->str, name);
 		if (p && *p)
 			return (p);
-		env_node = env_node->next;
+		node = node->next;
 	}
 	return (NULL);
 }
 
 /**
- * _mysetenv - Initializes a new environment variable,
- *             or modifies an existing one.
- * @info: Pointer to struct containing potential arguments.
- *
- * Return: Always 0.
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
  */
 int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		_eputs("Incorrect number of arguments\n");
+		_eputs("Incorrect number of arguements\n");
 		return (1);
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
@@ -54,10 +54,10 @@ int _mysetenv(info_t *info)
 }
 
 /**
- * _myunsetenv - Removes an environment variable.
- * @info: Pointer to struct containing potential arguments.
- *
- * Return: Always 0.
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * Return: Always 0
  */
 int _myunsetenv(info_t *info)
 {
@@ -65,7 +65,7 @@ int _myunsetenv(info_t *info)
 
 	if (info->argc == 1)
 	{
-		_eputs("Too few arguments.\n");
+		_eputs("Too few arguements.\n");
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
@@ -75,19 +75,19 @@ int _myunsetenv(info_t *info)
 }
 
 /**
- * populate_env_list - Populates the environment linked list.
- * @info: Pointer to struct containing potential arguments.
- *
- * Return: Always 0.
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
 int populate_env_list(info_t *info)
 {
-	list_t *env_node = NULL;
+	list_t *node = NULL;
 	size_t i;
 
 	for (i = 0; environ[i]; i++)
-		add_node_end(&env_node, environ[i], 0);
-	info->env = env_node;
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
 	return (0);
 }
 
--- errors.c
+++ errors.c
@@ -4,19 +4,19 @@
  * _eputs - prints an input string
  * @str: the string to be printed
  *
- * Return: void
+ * Return: Nothing
  */
 void _eputs(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        if (!str)
-                return;
-        while (str[i] != '\0')
-        {
-                _eputchar(str[i]);
-                i++;
-        }
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
 }
 
 /**
@@ -24,63 +24,63 @@ void _eputs(char *str)
  * @c: The character to print
  *
  * Return: On success 1.
- *         On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _eputchar(char c)
 {
-        static int i;
-        static char buf[WRITE_BUF_SIZE];
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
 
-        if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-        {
-                write(2, buf, i);
-                i = 0;
-        }
-        if (c != BUF_FLUSH)
-                buf[i++] = c;
-        return (1);
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
 
 /**
  * _putfd - writes the character c to given fd
  * @c: The character to print
- * @fd: The file descriptor to write to
+ * @fd: The filedescriptor to write to
  *
  * Return: On success 1.
- *         On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putfd(char c, int fd)
 {
-        static int i;
-        static char buf[WRITE_BUF_SIZE];
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
 
-        if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-        {
-                write(fd, buf, i);
-                i = 0;
-        }
-        if (c != BUF_FLUSH)
-                buf[i++] = c;
-        return (1);
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
 
 /**
  * _putsfd - prints an input string
  * @str: the string to be printed
- * @fd: the file descriptor to write to
+ * @fd: the filedescriptor to write to
  *
- * Return: the number of characters put
+ * Return: the number of chars put
  */
 int _putsfd(char *str, int fd)
 {
-        int i = 0;
+	int i = 0;
 
-        if (!str)
-                return (0);
-        while (*str)
-        {
-                i += _putfd(*str++, fd);
-        }
-        return (i);
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
 }
 
